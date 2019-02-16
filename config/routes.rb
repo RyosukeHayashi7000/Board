c8e66d624674fa64eda6dd176d6289d0a556bf54
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-17 01:13:41 +0900

    リソースベースルーティングに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'boards#index'
-  get "boards" => "boards#index"
-  get "boards/new" => "boards#new"
-  post "boards" => "boards#create"
-  get "boards/:id" => "boards#show"
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update] 
 end  
 

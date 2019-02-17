35f13df0b47182f0df566e677818fc3624631eb7
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-18 00:22:52 +0900

    Schema informationの追加、tagモデルとのアソシエーション追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,22 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  comment    :text(65535)
+#  image      :string(255)
+#  address    :string(255)
+#  mail       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Board < ApplicationRecord
   mount_uploader :image, ImageUploader
+  has_many :board_tag_relations, dependent: :delete_all
+  has_many :tags, through: :board_tag_relations 
 
   validates :name, presence: true, length: { maximum: 10}
   validates :title, presence: true, length: { maximum: 10}

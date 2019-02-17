8e71bfd9cf62cd6da08c4f8b3ea94d87a48a9237
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-17 17:25:32 +0900

    ページネーション用にindexアクションを変更

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,7 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
   
   def index
-    @boards = Board.all
+    @boards = Board.all.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def new

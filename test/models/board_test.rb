c8a8e82a87c141f1e26bf9ed2687540cd2bcb40a
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-18 00:31:56 +0900

    Schema Informationの追加

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  comment    :text(65535)
+#  image      :string(255)
+#  address    :string(255)
+#  mail       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase

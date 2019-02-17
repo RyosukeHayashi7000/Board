07f0006be75d1680de2bb9115bf58805d743bb58
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-17 19:02:35 +0900

    モデルの項目にvalidation機能を追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,12 @@
 class Board < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  validates :name, presence: true, length: { maximum: 10}
+  validates :title, presence: true, length: { maximum: 10}
+  validates :comment, presence: true, length: { maximum: 1000}
+  validates :address, presence: true, length: { maximum: 20}
+  validates :mail, presence: true, length: { maximum: 40}
+  
+
+
 end

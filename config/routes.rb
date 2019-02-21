9c3806cce02ba924ce3e7e23622c472c000e9010
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-21 22:04:19 +0900

    ユーザーログインぺージとマイページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'mypage' => 'users#me'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'boards#index'
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
 end  
 

0b6a38b8d67221b0d0dcbc67f26465a407857ec5
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-23 19:16:32 +0900

    投稿者の投稿一覧ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'boards#index'
   root 'home#index'
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
   resources :boards
 end  
 

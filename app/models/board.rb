b94bc0e9e9d3455b2020297aac681356eb3e010b
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-23 17:37:30 +0900

    user_idを追加（userモデルと紐づけ）

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,27 +3,27 @@
 # Table name: boards
 #
 #  id         :integer          not null, primary key
-#  name       :string(255)
-#  title      :string(255)
+#  address    :string(255)
 #  comment    :text(65535)
 #  image      :string(255)
-#  address    :string(255)
 #  mail       :string(255)
+#  title      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 
 class Board < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :board_tag_relations, dependent: :delete_all
-  has_many :tags, through: :board_tag_relations 
+  has_many :tags, through: :board_tag_relations
+  belongs_to :user
 
-  validates :name, presence: true, length: { maximum: 10}
+  
   validates :title, presence: true, length: { maximum: 10}
   validates :comment, presence: true, length: { maximum: 1000}
   validates :address, presence: true, length: { maximum: 20}
   validates :mail, presence: true, length: { maximum: 40}
+  validates :user_id, presence: true
   
-
-
 end

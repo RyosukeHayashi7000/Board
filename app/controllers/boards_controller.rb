ccdceeea1d2310456897f428def2e587cd56dd9f
github RyosukeHayashi7000 <yoyo30hr@gmail.com>
2019-02-23 17:32:04 +0900

    投稿者名をログインユーザー名に変更するため各アクションを変更

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,10 @@
 class BoardsController < ApplicationController
-  before_action :set_target_board, only: %i[show edit update destroy]
+  before_action :ensure_correct_user, only: %i[edit update destroy]
   
   def index
     @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
     @boards = @boards.order(created_at: :desc).page(params[:page]).per(5)
-  end
+  end  
 
   def new
     @board = Board.new(flash[:board])
@@ -12,6 +12,7 @@ class BoardsController < ApplicationController
 
   def create
     board = Board.new(board_params)
+    board.user_id = @current_user.id
     if board.save
       flash[:notice] = "「#{board.title}」の情報を投稿しました"
       redirect_to board
@@ -24,6 +25,8 @@ class BoardsController < ApplicationController
   end
 
   def show
+    @board = Board.find_by(id:params[:id])
+    @user = User.find_by(id: @board.user_id)
   end
   
   def edit
@@ -41,11 +44,19 @@ class BoardsController < ApplicationController
   
   private
   def board_params
-    params.require(:board).permit(:name, :title, :body, :image, :address, :mail, :comment, tag_ids: [])
+    params.require(:board).permit(:title, :body, :image, :address, :mail, :comment, tag_ids: [])
   end 
   
   def set_target_board
     @board = Board.find_by(id: params[:id])
-  end  
+  end
+  
+  def ensure_correct_user
+    @board = Board.find_by(id:params[:id])
+    if @board.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to boards_path
+    end
+  end     
 end
 
